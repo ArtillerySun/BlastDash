874ee1ad5009a6ea9cf9c07b61ad04fb8bc37c2a
ArtillerySun <e1297745@u.nus.edu>
2026-02-13 20:24:22 +0800

    update bomb with damage

--- Source/BlastDash/BD_Projectile.h
+++ Source/BlastDash/BD_Projectile.h
@@ -11,11 +11,6 @@ class BLASTDASH_API ABD_Projectile : public AActor
 {
 	GENERATED_BODY()
 	
-public:	
-	// Sets default values for this actor's properties
-	ABD_Projectile();
-	void OnSelfDestroy();
-
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -29,23 +24,42 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BlastDash|Physics")
 	float ExplosionForce = 50000.f;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(EditAnywhere, Category = "BlastDash|Logic")
+	float ExplosionDelayTime = 3.0f;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics")
+	UPROPERTY(EditAnywhere, Category = "BlastDash|Logic")
+	float BaseDamage = 50.0f;
+
+	UPROPERTY(EditAnywhere, Category = "BlastDash|Logic")
+	float ExplosionUpwardBias = 500.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BlastDash|Physics")
 	FVector Velocity; // Velocity
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BlastDash|Physics")
 	float Mass = 1.0f; // Mass
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BlastDash|Physics")
 	float DragForce = 0.1f; // Drag Force
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BlastDash|Physics")
 	float DampingFactor = 0.98f; // Damping Factor
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BlastDash|Physics")
 	FVector Gravity = FVector(0.f, 0.f, -980.f); // Gravity
 
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "BlastDash|Events")
+	void OnExplosionEffects();
+
+	float TimeElapsed = 0.0f;
+	bool bHasExploded = false;
+
+public:	
+
+	ABD_Projectile();
+	void OnSelfDestroy();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
 };
